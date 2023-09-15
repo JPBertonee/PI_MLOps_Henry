0dcd550fc2a0d3cd2192ba10e203431ac228fd08
JPBertonee <bertonejpb@gmail.com>
2023-09-15 18:11:40 +1200

    last

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-import funciones_api as funciones
 
 app = FastAPI()
 
 # http://127.0.0.1:8000
 
-@app.get(path="/", 
-         response_class=HTMLResponse,
-         tags=["Home"])
