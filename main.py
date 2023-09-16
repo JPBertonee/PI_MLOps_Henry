74f53c2b49c9b47a1823e8349fee7aba648fd3c5
JPBertonee <bertonejpb@gmail.com>
2023-09-16 12:16:20 +1200

    last

--- main.py
+++ main.py
@@ -218,9 +218,10 @@ def sentiment_analysis(anio):
     df_f6 (DataFrame): El DataFrame que contiene los datos de reseñas y sentimientos.
    
     '''
-    
+    df_f6['anio'] = df_f6['anio'].astype(int)
+
     # Filtramos el DataFrame según el año definido como argumento
-    df_filtered = df_f6.loc[df_f6['anio'] == anio]
+    df_filtered = df_f6[df_f6['anio'] == anio]
     
     df_filtered['sentimiento'] = df_f6['sentimiento'].astype(int)
     
